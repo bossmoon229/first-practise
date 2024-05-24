aab0ac039853cad06ad24ce3ea3fc02aa7d420b5
bossmoon229 <alinaignatich@gmail.com>
2024-05-24 15:21:34 +0300

    practic with ternary operator amd switch

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict"; 
+
+let num = 1000 ;
+
+// (num === 50) ? console.log('ok') : console.log('not ok');
+
+switch (num) {
+    case 49:
+        console.log('not ok');
+        break; 
+    case 100:
+        console.log('not ok');
+        break; 
+    case 50:
+        console.log('ok');
+        break; 
+    default:
+        console.log('not now');
+        break; 
+
+}

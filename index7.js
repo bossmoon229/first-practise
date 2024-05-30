4f9aa69ad582f9596e646a61ff9604555294e7d0
bossmoon229 <alinaignatich@gmail.com>
2024-05-30 11:29:49 +0300

    functions and objects

--- index7.js
+++ index7.js
@@ -12,7 +12,7 @@ genres
 P.S. Функции вызывать не обязательно */
 "use strict";
 
-let numberOfFilms = +prompt("Скільки фільмів ви вже подивились?",'');
+// let numberOfFilms = +prompt("Скільки фільмів ви вже подивились?",'');
 
 function start(){
     numberOfFilms = +prompt("Скільки фільмів ви вже подивились?",'');
@@ -25,7 +25,7 @@ function start(){
 // start();
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    // count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
@@ -41,7 +41,7 @@ function showMyDB(){
     }
 }
 
-showMyDB();
+// showMyDB();
 
 function writeYourGenres(){
     for(let i = 0; i < 3; i++){
@@ -56,13 +56,13 @@ function writeYourGenres(){
     }
 }
 
-writeYourGenres();
+// writeYourGenres();
 
 function rememberFilms(){
     for(let i = 0; i < 2; i++){
-        let a = prompt("Один із останніх вашиї переглянутих фільмів?",''),
+        let a = prompt("Один із останніх вашиї переглянутих фільмів?",'').trim(),
             b = prompt("Оцініь його",'');
-    
+        
             if( a!= null && b != null && a != '' && b!= '' && a.length <50){
                 personalMovieDB.movies[a] = b;
                 console.log("done");
@@ -73,8 +73,10 @@ function rememberFilms(){
     }
 
 }
+
 // rememberFilms();
 
+console.log(personalMovieDB);
 
 function detectPersonalLevel(){
     if(personalMovieDB.count < 10) {
@@ -93,3 +95,57 @@ function detectPersonalLevel(){
     
 }
 // detectPersonalLevel();
+
+// let a = 8/4;
+// let b = 13/4;
+// // console.log(parseInt(a));
+// // console.log(parseInt(b));
+// console.log(a);
+// console.log(parseInt(b+1));
+// console.log(a === parseInt(a));
+
+function calculateVolumeAndArea(numb) {
+    if(isNaN(numb) || numb < 0 || !(numb=== parseInt(numb))){
+        console.log("При вычислении произошла ошибка");
+    }
+    else{
+        
+   return (`Объем куба: ${numb * numb * numb}, площадь всей поверхности: ${6 * numb*numb}`);
+    }
+}
+function getCoupeNumber(numb) {
+    if(isNaN(numb) || !(numb === parseInt(numb)) || numb < 0){
+        console.log( "Ошибка. Проверьте правильность введенного номера места");
+    }
+    else if(numb == 0 || numb > 36){
+        console.log( "Таких мест в вагоне не существует");
+    }
+    else if(numb % 4 === 0){
+        console.log( numb/4);
+    }
+    else{
+    }
+    
+}
+// console.log(2 + 'fdsf');
+
+function fib(numb) {
+    if(numb <= 0 || typeof(numb)!== 'number' || isNaN(numb))return "";
+    if(numb == 1)return "0";
+    let numb1 = 0;
+    let numb2 = 1;
+    let res;
+    let result = "0 1"
+    for(let i = 2; i < numb; i++){
+        res = numb1 + numb2;
+        numb1 = numb2;
+        numb2 = res;
+        result+=" "+res;
+        // console.log(result);
+    }
+    return result;
+}
+// fib(3.3);
+// getCoupeNumber(12);
+
+
